fa92343315e7ecbcf14bfbb63e3e0289d1ecd944
husiyang <2803149178@qq.com>
2023-03-10 22:57:24 +1100

    read data from solver

--- WindowsRelease/SDK/main.py
+++ WindowsRelease/SDK/main.py
@@ -13,16 +13,43 @@ def finish():
 
 
 if __name__ == '__main__':
+    # 存放txt
+    f = open('input.txt', 'w')
+
     read_util_ok()
     finish()
     while True:
+        # 读取第一行帧序列和金钱数
         line = sys.stdin.readline()
+
+        # 读取工作台数量，来判断接下来要读几行数据
+        work_num = int(sys.stdin.readline())
+        temp = sys.stdout
+        sys.stdout = f
+        num = 1
+        print(line)
+        print(work_num)
+        while num <= work_num:
+            lineCache = sys.stdin.readline()
+            print(lineCache)
+            num += 1
+
+        # 读取机器人的状态数据
+        robot_line = 1
+        while robot_line <= 4:
+            lineCache = sys.stdin.readline()
+            print(lineCache)
+            robot_line += 1
+        sys.stdout = temp
+
+        # 官方代码，主要是获取帧数ID
         if not line:
             break
         parts = line.split(' ')
         frame_id = int(parts[0])
         read_util_ok()
 
+        # 官方代码，控制机器人的行动
         sys.stdout.write('%d\n' % frame_id)
         line_speed, angle_speed = 3, 1.5
         for robot_id in range(4):
